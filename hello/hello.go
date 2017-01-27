23f20d2d143a9fb22090d66f713bd27b73dceab0
Diego Alves <dfqalves@gmail.com>
2017-01-27 00:36:57 -0200

    metodo soma e teste unitaria

---
+++ hello/hello.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "github.com/diego-alves/go-test/math"
+)
+
+func main() {
+    fmt.Printf("Ola Mundo %d!!!\n", math.Sum(1,3))
+}
---
+++ math/sum_test.go
@@ -0,0 +1,19 @@
+package math
+
+import "testing"
+
+func TestSum(t *testing.T) {
+    cases := []struct {
+    	a, b, want int
+    }{
+        {1,2,3},
+	{3,4,7},
+	{5,6,11},
+    }
+    for _, c := range cases {
+        got := Sum(c.a, c.b)
+        if got != c.want {
+	    t.Errorf("Sum(%d,%d) == %d, want %d", c.a, c.b, got, c.want)
+	}
+    }
+}
